c70fc0602e0ebcfe8cc4c0b90d17288264e851bd
fenet-as <fenetasmamaw48@gmail.com>
2025-11-02 10:07:24 +0300

    Time: 179 ms (62.95%), Space: 40.5 MB (36.26%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -1,31 +1,25 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        '''
-        - sort position in reverse order, zip with speed
-        - calculate time (tgt - p)/v
-        [1,1,12,]
-        - st[]
-        if e > st[-1]:
-            f += 1
-
-
-        '''
-
         arr = list(zip(position,speed))
-        arr.sort(key=lambda x:x[0], reverse = True)
-        
-        t = []
+        arr.sort(key = lambda x:x[0], reverse = True)
 
+
+        t = []
         for e in arr:
-            ct = (target - e[0])/e[1]
-            t.append(ct)
-        
-        st = []
-        
+            p = e[0]
+            s = e[1]
+
+            tm = (target - p)/s
+            t.append(tm)
+
+
+
+        f = 1
+        mn = t[0]
         for e in t:
-            if st and e <= st[-1]:
-                continue
+            if e > mn:
+                f += 1
+                mn = e
             
-            st.append(e)
-        return len(st)
-        
+        return f
+             
