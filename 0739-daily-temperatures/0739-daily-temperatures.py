892448cbdf4c83001a7aa31a3fc227c23151481d
fenet-as <fenetasmamaw48@gmail.com>
2025-11-02 09:20:03 +0300

    Time: 103 ms (58.49%), Space: 26.6 MB (96%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,15 +1,15 @@
 class Solution:
     def dailyTemperatures(self, t: List[int]) -> List[int]:
-        st = []
-        sol = [0] * len(t)
+        
 
+        st = []
+        res = [0] * len(t)
         for i in range(len(t)-1,-1,-1):
-            while st and t[st[-1]] <= t[i]:
+            while st and t[i] >= t[st[-1]]:
                 st.pop()
             if st:
-                sol[i] = st[-1] - i
-            
+                res[i] = st[-1] - i
             st.append(i)
-        return sol
-        
-            
+        return res
+
+
