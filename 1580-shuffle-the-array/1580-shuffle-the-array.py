119df9d8032758b880e80c7d3d6dcf6192ddf8d7
fenet-as <fenetasmamaw48@gmail.com>
2025-11-12 21:59:05 +0300

    Time: 53 ms (83.57%), Space: 18.1 MB (20.83%) - LeetHub

---
+++ 1580-shuffle-the-array/1580-shuffle-the-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        res = []
+
+        i = 0
+        j = n
+
+        while j < len(nums):
+            res.append(nums[i])
+            res.append(nums[j])
+            i += 1
+            j += 1
+        return res
