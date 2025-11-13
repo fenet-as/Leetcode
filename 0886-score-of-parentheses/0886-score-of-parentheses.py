3ad0e62079d9b8c584e4ac850544f234d7cb5c2d
fenet-as <fenetasmamaw48@gmail.com>
2025-11-13 21:07:57 +0300

    Time: 0 ms (100%), Space: 17.9 MB (26.05%) - LeetHub

--- 0886-score-of-parentheses/0886-score-of-parentheses.py
+++ 0886-score-of-parentheses/0886-score-of-parentheses.py
@@ -1,52 +1,34 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
-        '''
-        ((()()))
-        ['o', 'o', 1, 1, 'c', 'c']
-        2 * 2 * ( 1 + 1) 
-        4 (1 + 1)
-        4 + 4
-        m = 4
-        sc = 4 + 4
-        
-        oo2 - 
-        11
-        o1
-        1
-        oo11
 
-        
         '''
-        n = []
-    
+        "(())"
+
+        if it opes and closes , i count score 1
+        any nesting will be multipl of 2  
+        '''
+
+        st = []
 
         for e in s:
-            if e == "(":
-                n.append('o')
-                # print(n)
+            if e == '(':
+                st.append(e)
             else:
-                if n and n[-1] == 'o':
-                    n.pop()
-                    n.append(1)
-                    # print(n)
-                elif n and n[-1] != 0:
-                    n.append('c')
-        
-                
-        
-        sc = 0
+                if st and st[-1] == '(':
+                    st.pop()
+                    st.append(1)
+                else:
+                    st.append(e)
+
+        ct = 0
         m = 1
-        for e in n:
-            if e == 'o':
+        for e in st:
+            if e == '(':
                 m *= 2
-            elif e == 'c':
+            elif e == ')':
                 m //= 2
             else:
-                sc += (m * e)
-            
-                
+                ct += m*e
+        return ct
 
         
-        # print(n)
-        # print(sc)
-        return sc
