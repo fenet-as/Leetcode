5970cb48f2a3c5a5bda794c76d01e1be58d03b24
fenet-as <fenetasmamaw48@gmail.com>
2025-11-17 20:09:21 +0300

    Time: 0 ms (100%), Space: 17.8 MB (65.34%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.py
+++ 1217-relative-sort-array/1217-relative-sort-array.py
@@ -1,24 +1,41 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        '''
-        left out arrays collect
-        common array count
-        
-        '''
-        la = []
-        ct = defaultdict(int)
+        st = set(arr2)
+        hm = defaultdict(int)
+        nue = []
         for e in arr1:
-            if e not in arr2:
-                la.append(e)
+            if e not in st:
+                nue.append(e)
             else:
-                ct[e] += 1
+                hm[e] += 1
         
         res = []
+
         for e in arr2:
-            for _ in range(ct[e]):
+            for _ in range(hm[e]):
                 res.append(e)
-        la.sort()
-        res.extend(la)
-        return res
+        if not nue:
+            return res 
+         
+
+        mx = max(nue)
+
+        ca = [0]*(mx+1)
+
+        for e in nue:
+            ca[e] += 1
+        
+        t = 0
+        for i in range(len(ca)):
+            while ca[i] > 0:
+                nue[t] = i
+                t += 1
+                ca[i] -= 1
         
 
+        res.extend(nue)
+        return res            
+
+        
+
+            
