99b7adf257b9f054dcf9d3828afeb24d1c6f4cbb
fenet-as <fenetasmamaw48@gmail.com>
2025-11-18 20:06:49 +0300

    Time: 123 ms (33.47%), Space: 26.9 MB (33.01%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        '''
-        keep mn while going
-        keep cn - mn while going
-        '''
         mn = float('inf')
-        mp = 0
+        mx = 0
+
+
         for e in prices:
             mn = min(mn,e)
-            cp = e - mn
-            mp = max(mp,cp)
-        return mp
+            mx = max(mx,e - mn)
+        return mx
