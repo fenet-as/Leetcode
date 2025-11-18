38ae3579df228a6dbab93a546496ce7614b09d58
fenet-as <fenetasmamaw48@gmail.com>
2025-11-18 22:13:45 +0300

    Time: 0 ms (100%), Space: 17.9 MB (7.95%) - LeetHub

--- 1137-height-checker/1137-height-checker.py
+++ 1137-height-checker/1137-height-checker.py
@@ -1,11 +1,10 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        ex = heights.copy()
-
-        ex.sort()
+        exp = heights.copy()
+        exp.sort()
 
         ct = 0
         for i in range(len(heights)):
-            if ex[i] != heights[i]:
+            if exp[i] != heights[i]:
                 ct += 1
         return ct
