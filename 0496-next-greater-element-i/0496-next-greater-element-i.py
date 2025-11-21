e3973e4d724f33a52210ed8cef744c8f61c61f7d
fenet-as <fenetasmamaw48@gmail.com>
2025-11-21 09:18:01 +0300

    Time: 0 ms (100%), Space: 18 MB (63.91%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,44 +1,21 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-            
-        '''
-        nums1 = [4,1,2], nums2 = [1,3,4,2]
 
-        2,4,3,1
+        st = []
 
         hm = {}
-        ce = st.pop()
-        while st and st[-1] < ce:
-            st.pop()
-        if st:
-            hm[ce] = st[-1]
-        else:
-            hm[ce] = -1
 
-        
-
-        '''
-
-        hm = {}
-        for e in nums2:
-            hm[e] = -1
-        nums2.reverse()
-        st = nums2
-
-
-        while st:
-            ce = st.pop()
-            ci = len(st)-1
-            while ci >= 0 and st[ci] < ce:
-                ci -= 1
-            if ci >= 0:
-                hm[ce] = st[ci]
+        for e in reversed(nums2):
+            while st and st[-1] < e:
+                st.pop()
+            if st:
+                hm[e] = st[-1]
             else:
-                hm[ce] = -1
+                hm[e] = -1
+                
+            st.append(e)
+
         res = []
         for e in nums1:
             res.append(hm[e])
         return res
-
-
-            
