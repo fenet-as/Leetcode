2d8088ff1c998adf393fc314c71821dd37c5e32a
fenet-as <fenetasmamaw48@gmail.com>
2025-11-29 09:06:33 +0300

    Time: 123 ms (54.16%), Space: 18.2 MB (23.18%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -4,13 +4,16 @@ class Solution:
 
         i = 0
         mx = 0
+        mxf = 0
         for j in range(len(s)):
             if s[j] not in hm:
                 hm[s[j]] = 1
             else:
                 hm[s[j]] += 1
 
-            while (j-i+1) - max(hm.values()) > k:
+            mxf = max(mxf,hm[s[j]])
+
+            while (j-i+1) - mxf > k:
                 hm[s[i]] -= 1
                 if hm[s[i]] == 0:
                     del hm[s[i]]
