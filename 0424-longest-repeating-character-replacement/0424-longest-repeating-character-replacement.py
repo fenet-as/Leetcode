ceaf6bb7396535bff12d93e5af44b2e194f29565
fenet-as <fenetasmamaw48@gmail.com>
2025-11-29 09:06:18 +0300

    Time: 183 ms (22.9%), Space: 18 MB (48.96%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,20 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        hm = {}
+
+        i = 0
+        mx = 0
+        for j in range(len(s)):
+            if s[j] not in hm:
+                hm[s[j]] = 1
+            else:
+                hm[s[j]] += 1
+
+            while (j-i+1) - max(hm.values()) > k:
+                hm[s[i]] -= 1
+                if hm[s[i]] == 0:
+                    del hm[s[i]]
+                i += 1
+            mx = max(mx,j-i+1)
+
+        return mx
