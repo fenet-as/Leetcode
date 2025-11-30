2df7df85f8a5b5c2dcf108efd5102d42a107e19d
fenet-as <fenetasmamaw48@gmail.com>
2025-11-30 09:04:12 +0300

    Time: 216 ms (28.9%), Space: 23.8 MB (12.72%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,19 +1,12 @@
+from collections import defaultdict
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        '''
-
-        '''
-        hm = {}
-
+        hm = defaultdict(int)
         i = 0
         mx = 0
         for j in range(len(fruits)):
-            if fruits[j] not in hm:
-                hm[fruits[j]] = 1
-            else:
-                hm[fruits[j]] += 1
-
-            while len(hm) > 2:
+            hm[fruits[j]] += 1
+            while len(hm)>2:
                 hm[fruits[i]] -= 1
                 if hm[fruits[i]] == 0:
                     del hm[fruits[i]]
@@ -21,4 +14,3 @@ class Solution:
             mx = max(mx,j-i+1)
         return mx
             
-
