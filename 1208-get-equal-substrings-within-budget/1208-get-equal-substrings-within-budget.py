a5233f28a708f477af2eb0eebb42f15730b89750
fenet-as <fenetasmamaw48@gmail.com>
2025-11-30 09:58:46 +0300

    Time: 30 ms (27.58%), Space: 18.2 MB (28.57%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
@@ -1,17 +1,15 @@
 class Solution:
     def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
-        '''
-        expand when cc < maxcost 
-        '''
         
         i = 0
-        c = 0
+        exp = 0
         mx = 0
         for j in range(len(s)):
-            c += abs( ord(s[j]) - ord(t[j]) )
-            while c > maxCost:
-                c -= abs(ord(s[i]) - ord(t[i]) )
+            exp += abs(ord(t[j]) - ord(s[j]) )
+            while exp > maxCost:
+                exp -= abs(ord(t[i]) - ord(s[i]) )
                 i += 1
             mx = max(mx,j-i+1)
         return mx
             
+
