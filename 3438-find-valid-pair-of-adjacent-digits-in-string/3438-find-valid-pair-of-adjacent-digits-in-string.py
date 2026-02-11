b10d4976be9721b0eaa9c1ceb699b5764fc3aa9c
fenet-as <fenetasmamaw48@gmail.com>
2026-02-11 13:58:40 +0300

    Update 3438-find-valid-pair-of-adjacent-digits-in-string.py

--- 3438-find-valid-pair-of-adjacent-digits-in-string/3438-find-valid-pair-of-adjacent-digits-in-string.py
+++ 3438-find-valid-pair-of-adjacent-digits-in-string/3438-find-valid-pair-of-adjacent-digits-in-string.py
@@ -4,7 +4,7 @@ class Solution:
         s_count = Counter(s)
 
         for i in range(len(s)-1):
-            # print(s_count[s[i]], s[i] ,s_count[s[i+1]]  ,s[i+1])
+            
             if (s_count[s[i]] == int(s[i])) and (s_count[s[i+1]] == int(s[i+1])):
                 if s[i] != s[i+1]:
                     return s[i:i+2]
